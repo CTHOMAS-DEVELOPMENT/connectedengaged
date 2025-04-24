2829b0221120a713b12b27487f13eea77668d336
chasbeen <charles.a.v.thomas@gmail.com>
2025-04-24 19:34:05 +0100

    **9933-Remote Video component integration-2+

--- src/FeedScreen/FeedScreen.js
+++ src/FeedScreen/FeedScreen.js
@@ -46,7 +46,7 @@ import {
   EnvelopePlusFill,
   EnvelopeSlashFill,
   Telephone,
-  TelephoneFill,
+  //TelephoneFill,
 } from "react-bootstrap-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { checkAuthorization } from "../system/authService";
